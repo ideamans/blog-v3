64a41a208829abb96d00f37c1aa1a09d86504573
Kunihiko Miyanaga <miyanaga@ideamans.com>
2025-05-05 16:48:43 +0900

    記事リストをモジュール化

---
+++ .vitepress/theme/ArticleList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { Post } from './posts.data.js'
+
+const props = defineProps<{
+  posts: Post[]
+}>()
+</script>
+
+<template>
+  <ul class="divide-y divide-gray-200 dark:divide-slate-200/5">
+    <li class="py-12" v-for="{ title, url, date, excerpt } of posts">
+      <article
+        class="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline"
+      >
+        <Date :date="date" />
+        <div class="space-y-5 xl:col-span-3">
+          <div class="space-y-6">
+            <h2 class="text-2xl leading-8 font-bold tracking-tight">
+              <a class="text-gray-900 dark:text-white" :href="url">{{
+                title
+              }}</a>
+            </h2>
+            <div
+              v-if="excerpt"
+              class="prose dark:prose-invert max-w-none text-gray-500 dark:text-gray-300"
+              v-html="excerpt"
+            ></div>
+          </div>
+          <div class="text-base leading-6 font-medium">
+            <a class="link" aria-label="read more" :href="url">続きを読む →</a>
+          </div>
+        </div>
+      </article>
+    </li>
+  </ul>
+</template>
--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -2,6 +2,7 @@
 import Date from './Date.vue'
 import { data as posts } from './posts.data.js'
 import { useData } from 'vitepress'
+import ArticleList from './ArticleList.vue'
 
 const { frontmatter } = useData()
 </script>
@@ -18,36 +19,6 @@ const { frontmatter } = useData()
         {{ frontmatter.subtext }}
       </p>
     </div>
-    <ul class="divide-y divide-gray-200 dark:divide-slate-200/5">
-      <li
-        class="py-12"
-        v-for="{ title, url, date, excerpt } of posts.slice(0, 10)"
-      >
-        <article
-          class="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline"
-        >
-          <Date :date="date" />
-          <div class="space-y-5 xl:col-span-3">
-            <div class="space-y-6">
-              <h2 class="text-2xl leading-8 font-bold tracking-tight">
-                <a class="text-gray-900 dark:text-white" :href="url">{{
-                  title
-                }}</a>
-              </h2>
-              <div
-                v-if="excerpt"
-                class="prose dark:prose-invert max-w-none text-gray-500 dark:text-gray-300"
-                v-html="excerpt"
-              ></div>
-            </div>
-            <div class="text-base leading-6 font-medium">
-              <a class="link" aria-label="read more" :href="url"
-                >続きを読む →</a
-              >
-            </div>
-          </div>
-        </article>
-      </li>
-    </ul>
+    <ArticleList :posts="posts.slice(0, 10)" />
   </div>
 </template>
